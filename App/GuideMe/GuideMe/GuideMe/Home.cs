ecffcb2f437333e2d016c2e44c3130446d27c292
José Francisco Martínez <francisco.mtzc@hotmail.com>
2018-06-08 22:50:06 -0500

    App init

---
+++ App/GuideMe/GuideMe/GuideMe/Home.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GuideMe
+{
+
+    public class Home : ContentPage
+    {
+
+        public Home()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            Image imgLogo = new Image
+            {
+                Source = "logo.png",
+            };
+          
+            StackLayout stacklayoutImage = new StackLayout
+            {
+                Padding = new Thickness(0, 5, 0, 5),
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Children =
+                {
+                    imgLogo,
+                }
+            };
+            Button btnFacebookLogin = new Button
+            {
+                Text = "Connect with Facebook",
+                BackgroundColor = Color.FromRgb(110, 214, 222),
+                TextColor = Color.White,
+                HeightRequest = 60,
+            };
+            btnFacebookLogin.Clicked += BtnFacebookLogin_Clicked;
+
+            StackLayout stacklayoutPrimaryContent = new StackLayout
+            {
+                Padding = 5,
+                BackgroundColor = Color.White,
+                Children =
+                {
+                    stacklayoutImage,
+                    btnFacebookLogin,
+                }
+            };
+            Content = stacklayoutPrimaryContent;
+        }
+
+        private void BtnFacebookLogin_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new FacebookLogin(), true);
+        }
+    }
+}
+
